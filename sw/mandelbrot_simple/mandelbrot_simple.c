051fb25d69594af0ab3dfd691709efc7fdd3d797
Rok Krajnc <rok.krajnc@gmail.com>
2020-08-28 13:49:14 +0200

    mandelbrot_simple.c updated with command-line arguments.

--- sw/mandelbrot_simple/mandelbrot_simple.c
+++ sw/mandelbrot_simple/mandelbrot_simple.c
@@ -6,33 +6,27 @@
 //// includes ////
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 
 
 //// defines ////
 // use grayscale palette
 //#define PALETTE_GREYSCALE
-// output filename
-#define FILENAME      "mandelbrot.ppm"
-// width of the output image
-#define IMG_WIDTH     720
-// height of the output image
-#define IMG_HEIGHT    480
-// left coordinate of the Mandelbrot set
-#define MANDELBROT_X0 -2.5
-// right coordinate of the Mandelbrot set
-#define MANDELBROT_X1 1.0
-// top coordinate of the Mandelbrot set
-#define MANDELBROT_Y0 1.0
-// bottom coordinate of the Mandelbrot set
-#define MANDELBROT_Y1 -1.0
-// maximum number of iterations
-#define NITERATIONS   256
-
-
-//// macros ////
-#define IMG_TO_MAN_X(coord) ({double _t = (double)coord/(double)IMG_WIDTH*(MANDELBROT_X1-MANDELBROT_X0) + MANDELBROT_X0; _t;})
-#define IMG_TO_MAN_Y(coord) ({double _t = (double)coord/(double)IMG_HEIGHT*(MANDELBROT_Y1-MANDELBROT_Y0) + MANDELBROT_Y0; _t;})
+// default output filename
+#define FILENAME        "mandelbrot.ppm"
+// default width of the output image
+#define IMG_WIDTH       720U
+// default height of the output image
+#define IMG_HEIGHT      480U
+// default maximum number of iterations
+#define NITERATIONS     256U
+// default Mandelbrot zoom
+#define MANDELBROT_ZOOM 1.2
+// default Mandelbrot cetner x coordinate
+#define MANDELBROT_CX   ((1.0+(-2.5))/2.0)
+// default Mandelbrot center y coordinate
+#define MANDELBROT_CY   ((1.0+(-1.0))/2.0)
 
 
 //// types ////
@@ -45,31 +39,93 @@ typedef struct {
 } rgb_t;
 
 
-//// main() ////
-int main()
+//// usage() ////
+void usage(char* progname)
 {
+  fprintf(stderr, "Usage: %s [-h] [-o mandelbrot.ppm] [-iw image_width] [-ih image_height] [-n niterations] [-cx x_coord] [-cy y_coord] [-z zoom]\n", progname);
+  fprintf(stderr, "  -h               - show this help\n");
+  fprintf(stderr, "  -iw image_width  - set output image width to image_width (default: %u)\n", IMG_WIDTH);
+  fprintf(stderr, "  -ih image_height - set output image height to image_height (default: %u)\n", IMG_HEIGHT);
+  fprintf(stderr, "  -n niterations   - set maximal iterations to niterations (default: %u)\n", NITERATIONS);
+  fprintf(stderr, "  -cx x_coord      - set Mandelbrot center x coordinate to x_coord (default: %f)\n", MANDELBROT_CX);
+  fprintf(stderr, "  -cy y_coord      - set Mandelbrot center y coordinate to y_coord (default: %f)\n", MANDELBROT_CY);
+  fprintf(stderr, "  -z zoom          - set Mandelbrot zoom to zoom (default: %f)\n", MANDELBROT_ZOOM);
+  exit(EXIT_FAILURE);
+}
+
+
+//// main() ////
+int main(int argc, char*argv[])
+{
+  // default values
+  char* filename  = FILENAME;
+  uint32_t img_w  = IMG_WIDTH;
+  uint32_t img_h  = IMG_HEIGHT;
+  uint32_t niter  = NITERATIONS;
+  double man_cx   = MANDELBROT_CX;
+  double man_cy   = MANDELBROT_CY;
+  double man_zoom = MANDELBROT_ZOOM;
+
+  // parse cmd args
+  int curpos = 1;
+  while (curpos < argc) {
+    if        (!strcmp(argv[curpos], "-h")) {
+      usage(argv[0]);
+    } else if (!strcmp(argv[curpos], "-o")) {\
+      curpos++;
+      filename = argv[curpos++];
+    } else if (!strcmp(argv[curpos], "-iw")) {\
+      curpos++;
+      img_w = strtoul(argv[curpos++], NULL, 0);
+    } else if (!strcmp(argv[curpos], "-ih")) {\
+      curpos++;
+      img_h = strtoul(argv[curpos++], NULL, 0);
+    } else if (!strcmp(argv[curpos], "-n")) {\
+      curpos++;
+      niter = strtoul(argv[curpos++], NULL, 0);
+    } else if (!strcmp(argv[curpos], "-cx")) {\
+      curpos++;
+      man_cx = strtod(argv[curpos++], NULL);
+    } else if (!strcmp(argv[curpos], "-cy")) {\
+      curpos++;
+      man_cy = strtod(argv[curpos++], NULL);
+    } else if (!strcmp(argv[curpos], "-z")) {\
+      curpos++;
+      man_zoom = strtod(argv[curpos++], NULL);
+    } else {
+      usage(argv[0]);
+    }
+  }
+
+  // calculate Mandelbrot coordinates
+  double man_x0 = man_cx - (double)img_w/(double)img_h*man_zoom;
+  double man_x1 = man_cx + (double)img_w/(double)img_h*man_zoom;
+  double man_y0 = man_cy - 1.0*man_zoom;
+  double man_y1 = man_cy + 1.0*man_zoom;
+
   // create a palette of colors
-  rgb_t palette[NITERATIONS];
-  #ifdef PALETTE_GREYSCALE
+  rgb_t* palette = NULL;
+  if ((palette = (rgb_t*)malloc(niter * sizeof(rgb_t))) == NULL) {
+    fprintf(stderr, "Can't allocate palette array, exiting.\n");
+    exit(EXIT_FAILURE);
+  }
   #pragma omp parallel for
-  for (int i=0; i<NITERATIONS; i++) {
+  for (int i=0; i<niter; i++) {
+    #ifdef PALETTE_GREYSCALE
     palette[i].r = 255 - (double)i/NITERATIONS * 255.0;
     palette[i].g = 255 - (double)i/NITERATIONS * 255.0;
     palette[i].b = 255 - (double)i/NITERATIONS * 255.0;
-  }
-  #else
-  #pragma omp parallel for
-  for (int i=0; i<NITERATIONS; i++) {
+    #else
     double t = (double)i/NITERATIONS;
     palette[i].r = (9.0*(1-t)*t*t*t*255.0);
     palette[i].g = (15.0*(1-t)*(1-t)*t*t*255.0);
     palette[i].b = (8.5*(1-t)*(1-t)*(1-t)*t*255.0);
+    #endif
   }
-  #endif
 
   // create image array
   uint32_t* iterations = NULL;
-  if ((iterations = (uint32_t*)malloc(IMG_WIDTH*IMG_HEIGHT * sizeof(uint32_t))) == NULL) {
+  if ((iterations = (uint32_t*)malloc(img_w*img_h * sizeof(uint32_t))) == NULL) {
     fprintf(stderr, "Can't allocate iterations array, exiting.\n");
     exit(EXIT_FAILURE);
   }
@@ -77,12 +133,12 @@ int main()
   // calculate the Mandelbrot set
   // iterate over all image rows
   #pragma omp parallel for ordered schedule(dynamic)
-  for (uint32_t img_y=0; img_y<IMG_HEIGHT; img_y++) {
-    double man_y = IMG_TO_MAN_Y(img_y);
+  for (uint32_t img_y=0; img_y<img_h; img_y++) {
+    double man_y = (double)(img_y)/(double)img_h*(man_y1-man_y0) + man_y0;
     // iterate over all image columns
-    for (uint32_t img_x=0; img_x<IMG_WIDTH; img_x++) {
+    for (uint32_t img_x=0; img_x<img_w; img_x++) {
       // convert image coordinates to Mandelbrot coordinates
-      double man_x = IMG_TO_MAN_X(img_x);
+      double man_x = (double)(img_x)/(double)img_w*(man_x1-man_x0) + man_x0;
       // initialize Zn to 0 + i0
       double zn_x = 0.0;
       double zn_y = 0.0;
@@ -91,7 +147,7 @@ int main()
       // initialize temporary variables
       double x2 = 0.0;
       double y2 = 0.0;
-      while (x2 + y2 <= 4.0 && niterations < NITERATIONS-1) {
+      while (x2 + y2 <= 4.0 && niterations < niter-1) {
         zn_y = 2*zn_x*zn_y + man_y;
         zn_x = x2 - y2 + man_x;
         x2   = zn_x*zn_x;
@@ -102,25 +158,25 @@ int main()
         niterations++;
       }
       // save number of iterations to iterations array
-      iterations[img_y*IMG_WIDTH+img_x] = niterations;
+      iterations[img_y*img_w+img_x] = niterations;
     }
   }
 
   // open output image file
   FILE* fp = NULL;
-  if ((fp  = fopen(FILENAME, "wb")) == NULL) {
+  if ((fp  = fopen(filename, "wb")) == NULL) {
     fprintf(stderr, "Can't open output file " FILENAME ", exiting.\n");
     exit(EXIT_FAILURE);
   }
 
   // write ppm image header
-  fprintf(fp, "P3\n%d %d\n255\n", IMG_WIDTH, IMG_HEIGHT);
+  fprintf(fp, "P3\n%d %d\n255\n", img_w, img_h);
 
   // convert number of iterations to a rgb value and write it to the output image file & collect statistics
   uint32_t min_iterations = UINT32_MAX;
   uint32_t max_iterations = 0;
   uint64_t sum_iterations = 0;
-  for (uint32_t i=0; i<IMG_WIDTH*IMG_HEIGHT; i++) {
+  for (uint32_t i=0; i<img_w*img_h; i++) {
     uint32_t niterations = iterations[i];
     fprintf(fp, "%d %d %d\n", palette[niterations].r, palette[niterations].g, palette[niterations].b);
     min_iterations = niterations < min_iterations ? niterations : min_iterations;
@@ -135,11 +191,14 @@ int main()
   free(iterations);
 
   // output stats
-  printf("Mandelbrot set calculated for %d points with %d max iterations.\n", IMG_WIDTH*IMG_HEIGHT, NITERATIONS);
+  printf("Mandelbrot set calculated with %d points & %d max iterations.\n", img_w*img_h, niter);
+  printf("X coords: %.8e  -  %.8e  -  %.8e\n", man_x0, man_cx, man_x1);
+  printf("Y coords: %.8e  -  %.8e  -  %.8e\n", man_y0, man_cy, man_y1);
+  printf("Zoom:     %.8e\n", man_zoom);
   printf("minimal iterations: %u\n", min_iterations);
   printf("maximal iterations: %u\n", max_iterations);
   printf("all iterations:     %lu\n", sum_iterations);
-  printf("average iter/pixel: %f\n", (double)sum_iterations/(double)(IMG_WIDTH*IMG_HEIGHT));
+  printf("average iter/pixel: %f\n", (double)sum_iterations/(double)(img_w*img_h));
 
   // exit
   exit(EXIT_SUCCESS);
