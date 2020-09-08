b510a5876362622f944dee1d866264c8ca3bf95a
Rok Krajnc <rok.krajnc@gmail.com>
2020-09-08 21:01:11 +0200

    updated sys pll.

--- rtl/clock/sys_pll.v
+++ rtl/clock/sys_pll.v
@@ -55,7 +55,7 @@ endmodule
 // Retrieval info: 	<generic name="gui_pll_mode" value="Integer-N PLL" />
 // Retrieval info: 	<generic name="gui_reference_clock_frequency" value="50.0" />
 // Retrieval info: 	<generic name="gui_channel_spacing" value="0.0" />
-// Retrieval info: 	<generic name="gui_operation_mode" value="direct" />
+// Retrieval info: 	<generic name="gui_operation_mode" value="normal" />
 // Retrieval info: 	<generic name="gui_feedback_clock" value="Global Clock" />
 // Retrieval info: 	<generic name="gui_fractional_cout" value="32" />
 // Retrieval info: 	<generic name="gui_dsm_out_sel" value="1st_order" />
