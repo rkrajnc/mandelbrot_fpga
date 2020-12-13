7e8c802da637b9edcc337ab2a6412541cdb8be7b
Rok Krajnc <rok.krajnc@gmail.com>
2020-12-13 21:08:34 +0100

    updated video_pipe_sync_top_tb module.

--- tb/video/video_pipe_sync_top_tb.v
+++ tb/video/video_pipe_sync_top_tb.v
@@ -11,12 +11,21 @@ module video_pipe_sync_top_tb();
 
 
 //// local parameters ////
-localparam CLK_HPER = 10;   // clock half-period
-localparam WIDTH    = 640;  // image width
-localparam HEIGHT   = 480;  // image height
-localparam CCW      = 8;    // color component width
-localparam MAW      = 19;   // memory address width
-localparam MDW      = 8;    // memory data width
+localparam CLK_HPER = 10;               // clock half-period
+localparam WIDTH    = 640;              // image width
+localparam HEIGHT   = 480;              // image height
+localparam NPIXELS  = WIDTH*HEIGHT;     // number of active pixels
+localparam CCW      = 8;                // color component width
+localparam IMAW     = $clog2(NPIXELS);  // memory address width
+localparam IMDW     = 8;                // memory data width
+localparam TW       = 80;               // console text width in characters
+localparam TH       = 2;                // console text height in characters
+localparam NCHARS   = TW*TH;            // number of characters in console
+localparam CMAW     = $clog2(NCHARS);   // console memory address width
+localparam CMDW     = 8;                // console memory data width
+
+localparam CONSOLE_EN = 1'b1;
+localparam BORDER_EN  = 1'b0;
 
 
 //// clock ////
@@ -51,10 +60,12 @@ always @ (posedge clk, posedge rst) begin
   end
 end
 
+
 //// testbench ////
 reg en = 0;
 
 initial begin
+  integer i;
   integer x;
   integer y;
 
@@ -62,11 +73,19 @@ initial begin
 
   // fill video index ram
   for (y=0; y<HEIGHT; y=y+1) begin
-    for(x=0; x<WIDTH; x=x+1) begin
-      DUT.video_index_ram.mem[y*WIDTH+x] = y;
+    for (x=0; x<WIDTH; x=x+1) begin
+      DUT.video_index_ram.mem[y*WIDTH+x] = x+y+100;
     end
   end
-  //for (i=0; i<256; i=i+1) DUT.video_clut_rom.mem[i] = i;
+
+  // fill console ram
+  i = 0;
+  for (y=0; y<TH; y=y+1) begin
+    for (x=0; x<TW; x=x+1) begin
+      DUT.video_text_console.console_text_ram.mem[i] = "a" + i;
+      i = i+1;
+    end
+  end
 
   // wait for reset
   $display("TB : waiting for reset ...");
@@ -93,32 +112,41 @@ end
 wire vid_active;
 wire vid_hsync;
 wire vid_vsync;
-wire [8-1:0] vid_r;
-wire [8-1:0] vid_g;
-wire [8-1:0] vid_b;
+wire [CCW-1:0] vid_r;
+wire [CCW-1:0] vid_g;
+wire [CCW-1:0] vid_b;
 
 video_pipe_sync_top #(
-  .CCW (CCW),  // color component width
-  .MAW (MAW), // memory address width
-  .MDW (MDW)  // memory data width
+  .CCW  (CCW  ),  // color component width
+  .TW   (TW   ),  // console text width in characters
+  .TH   (TH   ),  // console text height in characters
+  .CMAW (CMAW ),  // console memory address width
+  .CMDW (CMDW ),  // console memory data width
+  .IMAW (IMAW ), // memory address width
+  .IMDW (IMDW )  // memory data width
 ) DUT (
-  .clk            (clk        ),  // video clock
-  .clk_en         (clk_en     ),  // video clock enable
-  .rst            (rst        ),  // video clock reset
-  .en             (en         ),  // enable video pipe
-  .border_en      (1'b0       ),  // enable drawing of border
-  .console_en     (1'b1       ),  // enable textual console
-  .vram_clk_w     (1'b0       ),  // video memory write clock
-  .vram_clk_en_w  (1'b0       ),  // video memory clock enable
-  .vram_we        (1'b0       ),  // video memory write enable
-  .vram_adr_w     (19'b0      ),  // video memory write address
-  .vram_dat_w     (8'b0       ),  // video memory write data
-  .vid_active     (vid_active ),  // video active (not blanked)
-  .vid_hsync      (vid_hsync  ),  // video horizontal sync
-  .vid_vsync      (vid_vsync  ),  // video vertical sync
-  .vid_r          (vid_r      ),  // video red component
-  .vid_g          (vid_g      ),  // video green component
-  .vid_b          (vid_b      )   // video blue component
+  .clk            (clk          ),  // video clock
+  .clk_en         (clk_en       ),  // video clock enable
+  .rst            (rst          ),  // video clock reset
+  .en             (en           ),  // enable video pipe
+  .border_en      (BORDER_EN    ),  // enable drawing of border
+  .console_en     (CONSOLE_EN   ),  // enable textual console
+  .vram_clk_w     (1'b0         ),  // video memory write clock
+  .vram_clk_en_w  (1'b0         ),  // video memory clock enable
+  .vram_we        (1'b0         ),  // video memory write enable
+  .vram_adr_w     ({IMAW{1'b0}} ),  // video memory write address
+  .vram_dat_w     ({IMDW{1'b0}} ),  // video memory write data
+  .con_clk_w      (1'b0         ),  // video memory write clock
+  .con_clk_en_w   (1'b0         ),  // video memory clock enable
+  .con_we         (1'b0         ),  // video memory write enable
+  .con_adr_w      ({CMAW{1'b0}} ),  // video memory write address
+  .con_dat_w      ({CMDW{1'b0}} ),  // video memory write data
+  .vid_active     (vid_active   ),  // video active (not blanked)
+  .vid_hsync      (vid_hsync    ),  // video horizontal sync
+  .vid_vsync      (vid_vsync    ),  // video vertical sync
+  .vid_r          (vid_r        ),  // video red component
+  .vid_g          (vid_g        ),  // video green component
+  .vid_b          (vid_b        )   // video blue component
 );
 
 
