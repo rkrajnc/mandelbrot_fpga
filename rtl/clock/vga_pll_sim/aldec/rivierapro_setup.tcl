d70300849a03ebcc4f7f9d0c2b0d4ac7ddc83205
Rok Krajnc <rok.krajnc@gmail.com>
2021-01-02 19:59:03 +0100

    updated vga_clk PLL to 40MHz (800x600 resolution).

--- rtl/clock/vga_pll_sim/aldec/rivierapro_setup.tcl
+++ rtl/clock/vga_pll_sim/aldec/rivierapro_setup.tcl
@@ -1,5 +1,5 @@
 
-# (C) 2001-2020 Altera Corporation. All rights reserved.
+# (C) 2001-2021 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 18.0 614 win32 2020.09.05.22:33:22
+# ACDS 18.0 614 win32 2021.01.01.19:11:12
 # ----------------------------------------
 # Auto-generated simulation script rivierapro_setup.tcl
 # ----------------------------------------
--- rtl/clock/vga_pll_sim/synopsys/vcs/vcs_setup.sh
+++ rtl/clock/vga_pll_sim/synopsys/vcs/vcs_setup.sh
@@ -1,5 +1,5 @@
 
-# (C) 2001-2020 Altera Corporation. All rights reserved.
+# (C) 2001-2021 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 18.0 614 win32 2020.09.05.22:33:22
+# ACDS 18.0 614 win32 2021.01.01.19:11:12
 
 # ----------------------------------------
 # vcs - auto-generated simulation script
@@ -94,7 +94,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 18.0 614 win32 2020.09.05.22:33:22
+# ACDS 18.0 614 win32 2021.01.01.19:11:12
 # ----------------------------------------
 # initialize variables
 TOP_LEVEL_NAME="vga_pll"
