9adde1498936324d5649ca786d5ae54289968d33
Rok Krajnc <rok.krajnc@gmail.com>
2021-01-05 21:23:33 +0100

    mandelbrot_top: added multiple calc modules running in parallel; added stats; resolution is now runtime configurable.

--- rtl/mandelbrot/mandelbrot_top.v
+++ rtl/mandelbrot/mandelbrot_top.v
@@ -4,37 +4,42 @@
 
 
 module mandelbrot_top #(
-  parameter FPW       = 2*27,             // bitwidth of fixed-point numbers
-  parameter MAXITERS  = 256,              // max number of iterations
-  parameter IW        = $clog2(MAXITERS), // width of iteration output value
-  parameter AW        = 12,               // address width
-  parameter CW        = 12,               // screen coordinates counters width
-  parameter FD        = 8                 // fifo depth
+  parameter NCALC     = 8,                  // number of mandelbrot calculation engines
+  parameter FPW       = 2*27,               // bitwidth of fixed-point numbers
+  parameter MAXITERS  = 256,                // max number of iterations
+  parameter IW        = $clog2(MAXITERS),   // width of iteration output value
+  parameter AW        = 12,                 // address width
+  parameter CW        = 12,                 // screen coordinates counters width
+  parameter FD        = 8                   // fifo depth
 )(
   // system
-  input  wire                   clk,      // clock
-  input  wire                   clk_en,   // clock enable
-  input  wire                   rst,      // reset
+  input  wire                   clk,        // clock
+  input  wire                   clk_en,     // clock enable
+  input  wire                   rst,        // reset
   // control
-  input  wire                   init,     // enable mandelbrot calculation (posedge sensitive!)
-  output wire                   done,     // mandelbrot engine done
+  input  wire                   init,       // enable mandelbrot calculation (posedge sensitive!)
+  output wire                   done,       // mandelbrot engine done
   // config
-  input  wire signed [ FPW-1:0] man_x0,   // leftmost Mandelbrot coordinate
-  input  wire signed [ FPW-1:0] man_y0,   // uppermost Mandelbrot coordinate
-  input  wire signed [ FPW-1:0] man_xs,   // Mandelbrot x step
-  input  wire signed [ FPW-1:0] man_ys,   // Mandelbrot y step
+  input  wire        [  CW-1:0] hres,       // horizontal resolution
+  input  wire        [  CW-1:0] vres,       // vertical resolution
+  input  wire        [  32-1:0] npixels,    // number of pixels (for stats)
+  input  wire signed [ FPW-1:0] man_x0,     // leftmost Mandelbrot coordinate
+  input  wire signed [ FPW-1:0] man_y0,     // uppermost Mandelbrot coordinate
+  input  wire signed [ FPW-1:0] man_xs,     // Mandelbrot x step
+  input  wire signed [ FPW-1:0] man_ys,     // Mandelbrot y step
+  // stats output
+  output reg         [  32-1:0] niters,     // number of all iterations
+  output reg         [  32-1:0] timer,      // timer
+  output reg                    stats_done, // statistics done
   // mandelbrot output
-  input  wire                   out_rdy,  // output ready to receive (ack)
-  output wire                   out_vld,  // output valid
-  output wire        [  IW-1:0] out_dat,  // number of iterations
-  output wire        [  AW-1:0] out_adr   // mandelbrot coordinate address output
+  input  wire                   out_rdy,    // output ready to receive (ack)
+  output wire                   out_vld,    // output valid
+  output wire        [  IW-1:0] out_dat,    // number of iterations
+  output wire        [  AW-1:0] out_adr     // mandelbrot coordinate address output
 );
 
 
 //// mandelbrot coordinates ////
-localparam [CW-1:0] HRES = 'd799; // TODO!
-localparam [CW-1:0] VRES = 'd599;
-
 wire            coord_rdy;
 wire            coord_vld;
 wire [ FPW-1:0] x;
@@ -51,8 +56,8 @@ mandelbrot_coords #(
   .rst      (rst      ),  // reset
   .init     (init     ),  // initialize coord engine
   .done     (done     ),  // coord engine done
-  .hres     (HRES     ),  // horizontal resolution
-  .vres     (VRES     ),  // vertical resolution
+  .hres     (hres     ),  // horizontal resolution
+  .vres     (vres     ),  // vertical resolution
   .man_x0   (man_x0   ),  // leftmost Mandelbrot coordinate
   .man_y0   (man_y0   ),  // uppermost Mandelbrot coordinate
   .man_xs   (man_xs   ),  // Mandelbrot x step
@@ -99,36 +104,123 @@ sync_fifo #(
 );
 
 
+//// stream_distributor ////
+wire sd_in_vld;
+wire sd_in_rdy;
+wire [ NCALC-1:0] sd_out_vld;
+wire [ NCALC-1:0] sd_out_rdy;
+wire [   FDW-1:0] sd_out_dat;
+
+assign sd_in_vld = !fifo_empty;
+assign fifo_rd_en = sd_in_rdy && !fifo_empty;
+
+stream_distributor #(
+  .NS (NCALC),  // number of sinks
+  .DW (FDW)     // data width
+) mandelbrot_sd (
+  .clk      (clk        ),  // clock
+  .clk_en   (clk_en     ),  // clock enable
+  .rst      (rst        ),  // reset
+  .in_vld   (sd_in_vld  ),  // input valid
+  .in_rdy   (sd_in_rdy  ),  // input ack
+  .in_dat   (fifo_out   ),  // input data
+  .out_vld  (sd_out_vld ),  // output valid
+  .out_rdy  (sd_out_rdy ),  // output ack
+  .out_dat  (sd_out_dat )   // output data
+);
+
+
 //// mandelbrot calculate ////
-wire            calc_vld;
-wire            calc_rdy;
 wire [ FPW-1:0] calc_x;
 wire [ FPW-1:0] calc_y;
 wire [  AW-1:0] calc_adr;
 
-assign calc_vld   = !fifo_empty;
-assign fifo_rd_en = calc_rdy && !fifo_empty;
-assign {calc_adr, calc_y, calc_x} = fifo_out;
+assign {calc_adr, calc_y, calc_x} = sd_out_dat;
+
+// TODO!
+wire [NCALC-1:0] sc_in_vld;
+wire [NCALC-1:0] sc_in_rdy;
+wire [NCALC*IW-1:0] calc_iters;
+wire [NCALC*AW-1:0] calc_adrs;
 
 mandelbrot_calc #(
   .MAXITERS (MAXITERS), // max number of iterations
   .IW       (IW),       // width of iteration vars
   .FPW      (FPW),      // bitwidth of fixed-point numbers
   .AW       (AW)        // address width
-) mandelbrot_calc_wrap (
-  .clk      (clk      ),  // clock
-  .clk_en   (clk_en   ),  // clock enable
-  .rst      (rst      ),  // reset
-  .in_vld   (calc_vld ),  // input valid
-  .in_rdy   (calc_rdy ),  // input ack
-  .x_man    (calc_x   ),  // mandelbrot x coordinate
-  .y_man    (calc_y   ),  // mandelbrot y cooridnate
-  .adr_i    (calc_adr ),  // mandelbrot coordinate address input
-  .out_rdy  (out_rdy  ),  // output ack
-  .out_vld  (out_vld  ),  // output valid
-  .niter    (out_dat  ),  // number of iterations
-  .adr_o    (out_adr  )   // mandelbrot coordinate address output
+) mandelbrot_calc_wrap[NCALC-1:0] (
+  .clk      (clk        ),  // clock
+  .clk_en   (clk_en     ),  // clock enable
+  .rst      (rst        ),  // reset
+  .in_vld   (sd_out_vld ),  // input valid
+  .in_rdy   (sd_out_rdy ),  // input ack
+  .x_man    (calc_x     ),  // mandelbrot x coordinate
+  .y_man    (calc_y     ),  // mandelbrot y cooridnate
+  .adr_i    (calc_adr   ),  // mandelbrot coordinate address input
+  .out_rdy  (sc_in_rdy  ),  // output ack
+  .out_vld  (sc_in_vld  ),  // output valid
+  .niter    (calc_iters ),  // number of iterations
+  .adr_o    (calc_adrs  )   // mandelbrot coordinate address output
 );
+
+wire [NCALC-1:0][AW+IW-1:0] calc_data;
+
+genvar d;
+generate for (d=0; d<NCALC; d=d+1)  begin : SC_DAT_BLK
+  assign calc_data[d] = {calc_iters[(d+1)*IW-1:d*IW], calc_adrs[(d+1)*AW-1:d*AW]};
+end endgenerate
+
+
+//// stream collector ////
+localparam SCW = AW+IW;
+
+wire [SCW-1:0] sc_out_dat;
+
+stream_collector #(
+  .NS (NCALC),  // number of sinks
+  .DW (SCW)     // data width
+) mandelbrot_sc (
+  .clk      (clk        ),  // clock
+  .clk_en   (clk_en     ),  // clock enable
+  .rst      (rst        ),  // reset
+  .in_vld   (sc_in_vld  ),  // input valid
+  .in_rdy   (sc_in_rdy  ),  // input ack
+  .in_dat   (calc_data  ),  // input data
+  .out_vld  (out_vld    ),  // output valid
+  .out_rdy  (out_rdy    ),  // output ack
+  .out_dat  (sc_out_dat )   // output data
+);
+
+assign {out_dat, out_adr} = sc_out_dat;
+
+
+//// stats ////
+reg           init_r;
+wire          init_posedge;
+reg  [32-1:0] niters_r;
+reg  [32-1:0] pixel_cnt;
+
+assign init_posedge = init && ~init_r;
+
+always @ (posedge clk) begin
+  if (clk_en) begin
+    init_r <= #1 init;
+    timer <= #1 stats_done ? timer : timer + 'd1;
+    if (init_posedge) begin
+      pixel_cnt   <= #1 npixels;
+      timer       <= #1 'd0;
+      niters_r    <= #1 'd0;
+      stats_done  <= #1 1'b0;
+    end else if (out_vld && out_rdy) begin
+      pixel_cnt   <= #1 pixel_cnt - 'd1;
+      niters_r    <= #1 niters_r + out_dat;
+    end else if (~|pixel_cnt) begin
+      pixel_cnt   <= #1 npixels;
+      niters      <= #1 niters_r;
+      stats_done  <= #1 1'b1;
+    end
+  end
+end
 
 
 endmodule
